0729e4d32e8015de5591667e08eb0e675dd92a07
Kevixx <kevoboc@gmai.com>
2022-11-28 16:37:13 +0100

    V1.1 Deleted unnecessary attributes from 'profiles' table

--- Database/Database_SemesterProject.sql
+++ Database/Database_SemesterProject.sql
@@ -2,42 +2,33 @@ CREATE SCHEMA IF NOT EXISTS gaming_application_database;
 
 SET SCHEMA 'gaming_application_database';
 
-CREATE DOMAIN string as varchar(50);
-CREATE DOMAIN price_format as decimal(6, 2);
-
 CREATE TABLE profiles
 (
-    profile_name  string primary key not null,
-    first_name    string             not null,
-    last_name     string             not null,
-    date_of_birth date               not null,
-    street_name   string             not null,
-    house_number  smallint           not null,
-    city          string             not null,
-    zip_code      smallint           not null,
-    country       string             not null,
-    password      string              not null
+    profile_name varchar(50) primary key not null,
+    country      varchar(50)             not null,
+    address      varchar(50)             not null,
+    password     varchar(50)             not null
 );
 
 CREATE TABLE emails
 (
-    profile_name string primary key not null references profiles (profile_name) on update cascade,
-    email        string             not null check (email like '%@%.%') unique
+    profile_name varchar(50) primary key not null references profiles (profile_name) on update cascade,
+    email        varchar(50)             not null
 );
 
 CREATE TABLE games
 (
     game_id     serial primary key,
-    title       string       not null,
-    description varchar(500) not null,
-    price       price_format not null
+    title       varchar(50)   not null,
+    description varchar(500)  not null,
+    price       decimal(6, 2) not null
 );
 
 CREATE TABLE transactions
 (
     order_id         serial primary key,
-    game_id          serial       not null references games (game_id) on update cascade,
-    profile_name     string       not null references profiles (profile_name) on update cascade,
-    date_of_purchase date         not null,
-    purchase_price   price_format not null
+    game_id          serial        not null references games (game_id) on update cascade,
+    profile_name     varchar(50)   not null references profiles (profile_name) on update cascade,
+    date_of_purchase date          not null,
+    purchase_price   decimal(6, 2) not null
 );
