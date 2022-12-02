5622d8b0bd4c748eaae9bf4884a603b1a5abc4c8
Kevixx <kevoboc@gmai.com>
2022-12-02 20:40:13 +0100

    V2 new tables, fixed violations

--- Database/Database_SemesterProject.sql
+++ Database/Database_SemesterProject.sql
@@ -2,7 +2,8 @@ CREATE SCHEMA IF NOT EXISTS gaming_application_database;
 
 SET SCHEMA 'gaming_application_database';
 
-CREATE TABLE profiles
+--USERS TABLE
+CREATE TABLE users
 (
     email        varchar(50) primary key not null,
     country      varchar(50)             not null,
@@ -11,46 +12,48 @@ CREATE TABLE profiles
     password     varchar(50)             not null,
     isAdmin      boolean                 not null
 );
+--USERS TABLE END
 
-
+--GAMES TABLES
 CREATE TABLE games
 (
-    game_id     serial primary key,
-    title       varchar(50)   not null,
-    description varchar(500)  not null,
-    genre varchar(50) not null,
-    price       decimal(6, 2) not null
+    game_id serial primary key,
+    title   varchar(50)   not null,
+    price   decimal(6, 2) not null
 );
 
+CREATE TABLE descriptions
+(
+    game_id     serial primary key references games (game_id),
+    description varchar(500)
+);
+
+CREATE TABLE genres
+(
+    game_id serial references games (game_id),
+    genre   varchar(50) not null,
+    primary key (game_id, genre)
+);
+--GAMES TABLES END
+
+--TRANSACTION TABLES
 CREATE TABLE transactions
 (
     order_id         serial primary key,
-    game_id          serial        not null references games (game_id) on update cascade,
-    email            varchar(50)   not null references profiles (email) on update cascade,
+    email            varchar(50)   not null references users (email) on update cascade,
     date_of_purchase date          not null,
-    purchase_price   decimal(6, 2) not null
+    purchased_price  decimal(6, 2) not null
 );
 
-INSERT INTO games(game_id,title, genre, description, price)
-values(1,'WRC Generations', 'Racing',
-       'In the mythical continent of Westeros, several powerful..', 20.2),
-    (2,'Super Mario', 'Fantastic',
-       'In the mythical continent of Westeros, several powerful..', 20.2),
-    (3,'Grand Theft Auto', 'Action',
-       'In the mythical continent of Westeros, several powerful..', 20.2),
-    (4,'Street Fighter', 'Fighter',
-       'In the mythical continent of Westeros, several powerful..', 20.2),
-    (5,'Denmark Drift', 'Racing',
-       'In the mythical continent of Westeros, several powerful..', 20.2),
-    (6,'Gems of War', 'Strategy',
-       'In the mythical continent of Westeros, several powerful..', 20.2),
-    (7,'Need for Speed', 'Fantastic',
-       'In the mythical continent of Westeros, several powerful..', 20.2),
-    (8,'Battlefront', 'Shooter',
-       'In the mythical continent of Westeros, several powerful..', 20.2);
+CREATE TABLE games_in_transaction
+(
+    game_id  serial references games(game_id),
+    order_id serial references transactions (order_id),
+    primary key (game_id, order_id)
+);
+--TRANSACTION TABLES END
 
-DELETE FROM games
-WHERE game_id = 46;
+
 
 
 
