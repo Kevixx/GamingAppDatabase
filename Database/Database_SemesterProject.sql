07fe1b69636e3f30526143411669c69b49a6be67
Kevixx <kevoboc@gmai.com>
2022-12-08 13:05:55 +0100

    V2.4 change tables names to singular

--- Database/Database_SemesterProject.sql
+++ Database/Database_SemesterProject.sql
@@ -3,56 +3,70 @@ CREATE SCHEMA IF NOT EXISTS gaming_application_database;
 SET SCHEMA 'gaming_application_database';
 
 --USERS TABLE
-CREATE TABLE users
+CREATE TABLE "user"
 (
     email     varchar(50) primary key not null,
     country   varchar(50)             not null,
     address   varchar(50)             not null,
     user_name varchar(50)             not null,
-    password  varchar(50)             not null,
+    "password"  varchar(50)             not null,
     isAdmin   boolean                 not null
 );
 --USERS TABLE END
 
 --GAMES TABLES
-CREATE TABLE games
+CREATE TABLE game
 (
     game_id serial primary key,
     title   varchar(50)   not null,
     price   decimal(6, 2) not null
 );
 
-CREATE TABLE descriptions
+CREATE TABLE description
 (
-    game_id     serial primary key references games (game_id),
+    game_id     serial primary key references game (game_id),
     description varchar(500)
 );
 
-CREATE TABLE genres
+CREATE TABLE genre
 (
-    game_id serial references games (game_id),
+    game_id serial references game (game_id),
     genre   varchar(50) not null,
     primary key (game_id, genre)
 );
 --GAMES TABLES END
 
 --TRANSACTION TABLES
-CREATE TABLE transactions
+CREATE TABLE transaction
 (
     transaction_id   serial primary key,
-    email            varchar(50) not null references users (email) on update cascade,
+    email            varchar(50) not null references "user" (email) on update cascade,
     date_of_purchase date        not null
 );
 
-CREATE TABLE games_in_transaction
+CREATE TABLE game_in_transaction
 (
-    game_id        serial references games (game_id),
-    transaction_id serial references transactions (transaction_id),
+    game_id         serial references game (game_id),
+    transaction_id  serial references transaction (transaction_id),
+    purchased_price decimal(6, 2) not null,
     primary key (game_id, transaction_id)
 );
 --TRANSACTION TABLES END
 
 
-
+-- SELECT git.game_id
+-- FROM games
+--          inner join games_in_transaction git on games.game_id = git.game_id
+-- WHERE title like '%alo%'
+-- group by git.game_id;
+--
+-- SELECT gt.game_id, g.title, d.description, genre, gt.purchased_price
+-- FROM games_in_transaction gt
+--          join transactions t on t.transaction_id = gt.transaction_id
+--          join games g on gt.game_id = g.game_id
+--          join descriptions d on g.game_id = d.game_id
+-- join genres g2 on g.game_id = g2.game_id
+-- WHERE email = 'kevoboc@yahoo.com'
+-- GROUP BY gt.game_id, g.title, d.description, genre, purchased_price;
 
 
